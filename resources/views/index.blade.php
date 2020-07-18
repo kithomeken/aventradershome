071cb79dd30c09298e642f4a38cfec4ceb70a607
Kennedy Kithome <kithomeken@gmail.com>
2020-07-18 10:33:50 +0300

    Aven Traders Init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52115242-17"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-52115242-17');
+    </script>
+
+    
+
+
+
+
+</head>
