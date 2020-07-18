ec6f6d9559e85b2f724165072ff7f41617c2950b
Kennedy Kithome <kithomeken@gmail.com>
2020-07-18 16:28:32 +0300

    Nav Top Fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -119,7 +119,7 @@
     </div>
 
     <div class="navbar-container bg-light">
-        <nav class="navbar navbar-expand-lg navbar-light py-2" data-sticky="top">
+        <nav class="navbar navbar-expand-lg navbar-light position-fixed scrolled py-2" data-sticky="top">
             <div class="container">
                 <a class="navbar-brand navbar-brand-dynamic-color fade-page" href="index.html">
                     <h3 class="display-6 mb-0">
